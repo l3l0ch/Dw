910dfa99b8d74e4837b74c3419210ab35306fb73
l3l0ch <mateuskc11@gmail.com>
2024-03-25 08:03:48 -0300

    aula js

---
+++ aulajs0/ex-01.js
@@ -0,0 +1,32 @@
+//Saída de dados
+console.log("Hello, World!")
+//boolean
+console.log(true);
+console.log(false);
+console.log(typeof(false))
+//null
+console.log(null);
+//underfined
+a = [1,2,4]
+console.log(undefined);
+console.log(a[4]);
+console.log(typeof(a));
+console.log(a[2]);
+console.log(a[4]);
+// number 
+console.log(typeof(15));
+console.log(typeof(15.15));
+console.log(1515e2); // deslocamento de vírgula a direita
+console.log(1515e-2); //(15.15) deslocamento de vírgula a esquerda
+
+console.log(NaN); // numeros invalidos geram not a number (NaN).
+
+console.log((-2) ** (1/2));
+
+console.log(Infinity);
+
+
+// bigInt -> qualquer numero no final com n 
+console.log(typeof(12312321n));
+console.log(Number.MAX_SAFE_INTEGER)
+
