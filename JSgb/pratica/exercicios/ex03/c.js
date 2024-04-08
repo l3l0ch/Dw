36fb1494857090548ede8bd94ae70ae4b63fa0d2
l3l0ch <mateuskc11@gmail.com>
2024-04-08 14:26:20 -0300

    criando tela de login

--- JSgb/pratica/exercicios/ex03/c.js
+++ JSgb/pratica/exercicios/ex03/c.js
@@ -1,14 +1,16 @@
 function entrar(){
     
-    email='admin@gmail.com'
-    senha='1234'
+    admin = {
+    "email":'admin@gmail.com',
+    "senha":'1234'
+    }
 
     let acess_email = document.getElementById('mail');
     let acess_senha = document.getElementById('password');
 
     var date = new Date()
 
-    if (acess_email.value == email && acess_senha.value){
+    if (acess_email.value == admin.email && acess_senha.value == admin.senha){
         alert(`Usuário correto, você logou no sistema às ${date.getHours()} horas!`)
     }
     else{
