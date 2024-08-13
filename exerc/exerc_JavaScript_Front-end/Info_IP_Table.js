6c862e733c80bd940ace6b4c7b14b23591c2a5dd
l3l0ch <mateuskc11@gmail.com>
2024-08-12 22:26:15 -0300

    finalizado

--- exerc/exerc_JavaScript_Front-end/Info_IP_Table.js
+++ exerc/exerc_JavaScript_Front-end/Info_IP_Table.js
@@ -5,20 +5,25 @@ function pesquisar(){
     const url = `https://ipinfo.io/${ip.value}/json?`
 
     return fetch(url)
-        .then(
-        (response) => response.json()
-        )
-        .then(
-        ( function (jsonResponse){
+        .then((response) => response.json())
+        .then(jsonResponse => {
             console.log(jsonResponse)
-            pagina.innerHTML += ` 
-            <tr>
+            
+            const row = document.createElement('tr');
+
+            row.innerHTML = ` 
                 <td>${jsonResponse.ip}</td>
                 <td>${jsonResponse.org}</td>
                 <td>${jsonResponse.country}</td>
                 <td>${jsonResponse.city}</td>
-                <td class=close id=close> X </td>
-            </tr> 
-            `  
-        }))    
+                <td><button onclick="limpar(this)">X</button></td>
+            `;
+            pagina.appendChild(row);
+            ip.value = '';
+        }).catch((error) = console.log(error))
+    
  }
+ function limpar(button) {
+    const row = button.parentElement.parentElement;
+    row.remove();
+}
